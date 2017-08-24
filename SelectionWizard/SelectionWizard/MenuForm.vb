ee7e751cbf32c9d78ce11ca8b1169079c03b6cb3
deedee17 <deedee17@student.wintec.ac.nz>
2017-08-25 10:38:29 +1200

    exit button coded

--- SelectionWizard/SelectionWizard/MenuForm.vb
+++ SelectionWizard/SelectionWizard/MenuForm.vb
@@ -1,5 +1,9 @@
 ﻿Public Class MenuForm
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles cmdOpenStudentsForm.Click
+    Private Sub cmdExitApplication_Click(sender As Object, e As EventArgs) Handles cmdExitApplication.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub cmdOpenFacultyForm_Click(sender As Object, e As EventArgs) Handles cmdOpenFacultyForm.Click
 
     End Sub
 End Class
--- SelectionWizard/SelectionWizard/My Project/Application.Designer.vb
+++ SelectionWizard/SelectionWizard/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SelectionWizard.Login
+            Me.MainForm = Global.SelectionWizard.MenuForm
         End Sub
     End Class
 End Namespace
