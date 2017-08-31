c3d10ebc7a163ab30257898b7f9437332cee0997
deeps <deeps@cookers.com.au>
2017-08-31 22:21:43 +1200

    Configuration settings changed for Access

---
+++ SelectionWizard/SelectionWizard/CSE_DEPTDataSet.vb
@@ -0,0 +1,8 @@
+﻿Partial Class CSE_DEPTDataSet
+End Class
+
+Namespace CSE_DEPTDataSetTableAdapters
+
+    Partial Public Class LogInTableAdapter
+    End Class
+End Namespace
--- SelectionWizard/SelectionWizard/Login.Designer.vb
+++ SelectionWizard/SelectionWizard/Login.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class LoginForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class LoginForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -30,55 +30,60 @@ Partial Class LoginForm
         Me.cmdCancel = New System.Windows.Forms.Button()
         Me.txtUserName = New System.Windows.Forms.TextBox()
         Me.txtPassword = New System.Windows.Forms.TextBox()
-        Me.CSE_DEPTDataSet = New SelectionWizard.CSE_DEPTDataSet()
         Me.LogInBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CSE_DEPTDataSet = New SelectionWizard.CSE_DEPTDataSet()
         Me.LogInTableAdapter = New SelectionWizard.CSE_DEPTDataSetTableAdapters.LogInTableAdapter()
-        CType(Me.CSE_DEPTDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.LogInBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CSE_DEPTDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 21)
+        Me.Label1.Location = New System.Drawing.Point(16, 26)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(327, 25)
+        Me.Label1.Size = New System.Drawing.Size(413, 31)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Welcome to the CSE Department"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(19, 77)
+        Me.Label2.Location = New System.Drawing.Point(25, 95)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.Size = New System.Drawing.Size(87, 17)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "User Name :"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(19, 108)
+        Me.Label3.Location = New System.Drawing.Point(25, 133)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.Size = New System.Drawing.Size(77, 17)
         Me.Label3.TabIndex = 3
         Me.Label3.Text = "Password :"
         '
         'cmdOK
         '
-        Me.cmdOK.Location = New System.Drawing.Point(57, 151)
+        Me.cmdOK.Location = New System.Drawing.Point(76, 186)
+        Me.cmdOK.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdOK.Name = "cmdOK"
-        Me.cmdOK.Size = New System.Drawing.Size(75, 23)
+        Me.cmdOK.Size = New System.Drawing.Size(100, 28)
         Me.cmdOK.TabIndex = 5
         Me.cmdOK.Text = "OK"
         Me.cmdOK.UseVisualStyleBackColor = True
         '
         'cmdCancel
         '
-        Me.cmdCancel.Location = New System.Drawing.Point(150, 151)
+        Me.cmdCancel.Location = New System.Drawing.Point(200, 186)
+        Me.cmdCancel.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdCancel.Name = "cmdCancel"
-        Me.cmdCancel.Size = New System.Drawing.Size(75, 23)
+        Me.cmdCancel.Size = New System.Drawing.Size(100, 28)
         Me.cmdCancel.TabIndex = 6
         Me.cmdCancel.Text = "Cancel"
         Me.cmdCancel.UseVisualStyleBackColor = True
@@ -86,29 +91,31 @@ Partial Class LoginForm
         'txtUserName
         '
         Me.txtUserName.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.LogInBindingSource, "user_name", True))
-        Me.txtUserName.Location = New System.Drawing.Point(100, 73)
+        Me.txtUserName.Location = New System.Drawing.Point(133, 90)
+        Me.txtUserName.Margin = New System.Windows.Forms.Padding(4)
         Me.txtUserName.Name = "txtUserName"
-        Me.txtUserName.Size = New System.Drawing.Size(147, 20)
+        Me.txtUserName.Size = New System.Drawing.Size(195, 22)
         Me.txtUserName.TabIndex = 2
         '
         'txtPassword
         '
         Me.txtPassword.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.LogInBindingSource, "pass_word", True))
-        Me.txtPassword.Location = New System.Drawing.Point(100, 104)
+        Me.txtPassword.Location = New System.Drawing.Point(133, 128)
+        Me.txtPassword.Margin = New System.Windows.Forms.Padding(4)
         Me.txtPassword.Name = "txtPassword"
         Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.txtPassword.Size = New System.Drawing.Size(147, 20)
+        Me.txtPassword.Size = New System.Drawing.Size(195, 22)
         Me.txtPassword.TabIndex = 4
-        '
-        'CSE_DEPTDataSet
-        '
-        Me.CSE_DEPTDataSet.DataSetName = "CSE_DEPTDataSet"
-        Me.CSE_DEPTDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'LogInBindingSource
         '
         Me.LogInBindingSource.DataMember = "LogIn"
         Me.LogInBindingSource.DataSource = Me.CSE_DEPTDataSet
+        '
+        'CSE_DEPTDataSet
+        '
+        Me.CSE_DEPTDataSet.DataSetName = "CSE_DEPTDataSet"
+        Me.CSE_DEPTDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'LogInTableAdapter
         '
@@ -117,9 +124,9 @@ Partial Class LoginForm
         'LoginForm
         '
         Me.AcceptButton = Me.cmdOK
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(346, 209)
+        Me.ClientSize = New System.Drawing.Size(461, 257)
         Me.Controls.Add(Me.txtPassword)
         Me.Controls.Add(Me.txtUserName)
         Me.Controls.Add(Me.cmdCancel)
@@ -127,11 +134,12 @@ Partial Class LoginForm
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "LoginForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Login"
-        CType(Me.CSE_DEPTDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.LogInBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CSE_DEPTDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
